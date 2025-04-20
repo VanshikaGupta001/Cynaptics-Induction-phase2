7268fe96d73583e6d03a3dd88218765428e11243
VANSHIKA GUPTA <cse240001076@iiti.ac.in>
2025-04-20 10:54:23 +0530

    Update utils.py

--- config/utils.py
+++ config/utils.py
@@ -1,7 +1,7 @@
 import csv
 
 def load_case(serial_no):
-    with open('transformed_data.csv', newline='', encoding='utf-8') as csvfile:
+    with open('processed_data.csv', newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             if row['Serial No'] == str(serial_no):
